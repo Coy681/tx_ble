52f419ea08473eb771d69447ba4487b77a81f785
Tianxiang Huang <tianxiang.huang@telink-semi.com>
2024-12-11 20:18:02 +0800

    feature(module):malloc test code merge

--- module/txMalloc/txMalloc.c
+++ module/txMalloc/txMalloc.c
@@ -4,5 +4,125 @@
  *  Created on: 2024年12月10日
  *      Author: 12407
  */
+#include"common/txType.h"
+#include"common/txGeneral.h"
+#include"txMalloc.h"
+_u8 txMallocBuffer[TX_MALLOC_BUFFER_SIZE];
+
+typedef struct
+{
+    _u32 reserved;
+    _u32 totalSize;
+    _u32 unusedSize;
+    _u8* pStart;
+}txMalloc_t;
+txMalloc_t txMalloc;
+typedef struct
+{
+    _u8* previous;
+    _u8* next;
+    _u16 size;
+    _u16 usedFlag;
+}txMallocNode_t;
+
+void tx_malloc_init(_u8* buffer,_u32 size)
+{
+    txMalloc.totalSize =  size;
+    txMalloc.unusedSize = size-12;
+    txMalloc.pStart = buffer;
+    txMallocNode_t *pNode = (txMallocNode_t*)txMalloc.pStart;
+    pNode->previous = NULL;
+    pNode->usedFlag = 0;
+    pNode->size = size-12;
+    pNode->next = ((_u8*)&pNode->previous + 2);
+}
+
+_u8* tx_malloc(_u16 length)
+{
+    _u16 lenMalloc = ((length+3)&(~3));
+    txMallocNode_t *pNode = (txMallocNode_t*)txMalloc.pStart;
+    txMallocNode_t *pPreviousNode = pNode;
+    pNode = pNode->next;
+    while(pNode != NULL)
+    {
+        if(!pNode->usedFlag&&pNode->size>lenMalloc)
+        {
+            break;
+        }
+        pNode = pNode->next;
+        pPreviousNode = pPreviousNode->next;
+    }
+    if(pNode == NULL)
+    {
+        return NULL;
+    }
+    if(pNode->next == NULL)
+    {
+        //process,buffer tail
+        pNode->usedFlag = 1;
+        pNode->previous = pPreviousNode;
+        pNode->next = ((_u8*)&pNode->usedFlag+lenMalloc);
+        txMallocNode_t *pNext = (txMallocNode_t*)pNode->next;
+        pNext->size = pNode->size - 12 - lenMalloc;
+        pNode->size = lenMalloc;
+        return ((_u8*)&pNode->usedFlag+1);
+    }
+    else
+    {
+        //process,buffer middle
+        pNode->usedFlag = 1;
+        pNode->previous = pPreviousNode;
+        if(pNode->size>(lenMalloc+16))
+        {
+        	txMallocNode_t *pNext = (txMallocNode_t*)pNode->next;
+        	txMallocNode_t *pNew  = ((_u8*)&pNode->usedFlag+lenMalloc);
+        	pNew->usedFlag = 0;
+        	pNew->size = pNode->size - 12 - lenMalloc;
+        	pNew->next = pNext;
+        	pNode->next = pNew;
+        	pNext->previous = pNew;
+        	pNew->previous = pNode;
+        }
+        else
+        {
+            pNode->usedFlag = 1;
+        }
+        return ((_u8*)&pNode->usedFlag+2);
+    }
+}
+
+tx_malloc_e tx_free(_u8* PfreeNode)
+{
+	txMallocNode_t *pNode = (txMallocNode_t*)txMalloc->pStart;
+	txMallocNode_t *pPrevious = (txMallocNode_t*)pNode;
+	pNode = pNode->next;
+	while(pNode!=NULL)
+	{
+		pNode = pNode->next;
+		pPrevious = pPrevious->next;
+		if(((_u8*)pNode+12)==PfreeNode)
+		{
+            break;
+		}
+	}
+	if(pNode == NULL)
+	{
+        return TX_MALLOC_NOT_FROUND;
+	}
+	if(pNode->next == NULL)
+	{
+		pPrevious->next = NULL;
+		txMemsetByte((_u8*)pNode,0,12+pNode->size);
+	}
+	else
+	{
+		txMallocNode_t *pNext = pNode->next;
+		pPrevious->next = pNext;
+		pNext->previous = pPrevious;
+		pPrevious->size+=(12+pNode->size);
+		txMemsetByte((_u8*)pNode,0,12+pNode->size);
+	}
+	return TX_MALLOC_FREE_SUCCESS;
+}
 
 
--- module/txMalloc/txMalloc.h
+++ module/txMalloc/txMalloc.h
@@ -8,6 +8,14 @@
 #ifndef TXMALLOC_TXMALLOC_H_
 #define TXMALLOC_TXMALLOC_H_
 
+#define TX_MALLOC_BUFFER_SIZE     20*1024
 
+typedef enum
+{
+	TX_MALLOC_FREE_SUCCESS       = 0,
+	TX_MALLOC_INVALID_PARAMETER  = 1,
+	TX_MALLOC_NOT_FROUND         = 2,
+
+}tx_malloc_e;
 
 #endif /* TXMALLOC_TXMALLOC_H_ */
