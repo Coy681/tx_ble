444d14e5385d60c02ab0ff6761cfe2c928eb8908
Tianxiang <coy_china_681@163.com>
2025-01-08 16:11:32 +0800

    clean errors

--- system/task/event/event.h
+++ system/task/event/event.h
@@ -7,35 +7,35 @@
 
 #ifndef TASK_EVENT_EVENT_H_
 #define TASK_EVENT_EVENT_H_
-
-#include"task.h"
-
-typedef void(*pTaskInit_f)(void);
-typedef _u16(*pTaskProcess_f)(_u32,_u32);
-typedef struct txEvent txEvent_t;
-
-typedef struct txEvent
-{
-	txEvent_t*     next;
-	pTaskInit_f    init;
-	pTaskProcess_f process;
-    _u16           taskId;
-    _u16           taskPriority;
-    _u32           eventMask;
-}txTask_t;
-
-typedef enum
-{
-    TX_TASK_SUCCESS        = 0,
-    TX_TASK_NO_TASK        = 1,
-    TX_TASK_MEMORY_INVALID = 2,
-}txTask_e;
-
-void      tx_task_init(void);
-txTask_e  tx_task_add(pTaskInit_f init,pTaskProcess_f process,txTaskPriority_e priority);
-void      tx_task_start(void);
-txTask_t* tx_task_next_active(void);
-txTask_e  tx_task_set_event(_u16 taskId,_u32 event);
-txTask_e  tx_task_clear_event(_u16 taskId,_u32 event);
+//
+//#include"task.h"
+//
+//typedef void(*pTaskInit_f)(void);
+//typedef _u16(*pTaskProcess_f)(_u32,_u32);
+//typedef struct txEvent txEvent_t;
+//
+//typedef struct txEvent
+//{
+//	txEvent_t*     next;
+//	pTaskInit_f    init;
+//	pTaskProcess_f process;
+//    _u16           taskId;
+//    _u16           taskPriority;
+//    _u32           eventMask;
+//}txTask_t;
+//
+//typedef enum
+//{
+//    TX_TASK_SUCCESS        = 0,
+//    TX_TASK_NO_TASK        = 1,
+//    TX_TASK_MEMORY_INVALID = 2,
+//}txTask_e;
+//
+//void      tx_task_init(void);
+//txTask_e  tx_task_add(pTaskInit_f init,pTaskProcess_f process,txTaskPriority_e priority);
+//void      tx_task_start(void);
+//txTask_t* tx_task_next_active(void);
+//txTask_e  tx_task_set_event(_u16 taskId,_u32 event);
+//txTask_e  tx_task_clear_event(_u16 taskId,_u32 event);
 
 #endif /* TASK_EVENT_EVENT_H_ */
