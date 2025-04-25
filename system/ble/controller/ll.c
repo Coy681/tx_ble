622adb5609435f4e1086e668ed0ac5c2b65dab81
Tianxiang Huang <tianxiang.huang@telink-semi.com>
2025-04-25 16:17:04 +0800

    optimize(controller):ll optimize

--- system/ble/controller/ll.c
+++ system/ble/controller/ll.c
@@ -1,16 +1,24 @@
 #include"ll.h"
 
+
+/*********************************ll feature implementation**********************************/
 static _u64 ll_host_support_feature;
-
-
-controller_error_code_e ll_reset(void)
-{
-    //ll reset process
-    return SUCCESS;
-}
 
 _u64 ll_get_feature(void)
 {
 	return (LL_FEATURE_SUPPORT|ll_host_support_feature);
 }
 
+void ll_feature_reset(void)
+{
+	ll_host_support_feature = 0;
+}
+/******************************************ll reset*******************************************/
+controller_error_code_e ll_reset(void)
+{
+	//ll feature reset
+	ll_feature_reset();
+
+    //ll reset process
+    return SUCCESS;
+}
