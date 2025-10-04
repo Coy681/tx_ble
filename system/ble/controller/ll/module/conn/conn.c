e6aeea9f076bb34dbf81d84583cf69bc082b9023
coy <coy_china_681@163.com>
2025-10-04 09:58:21 +0800

    fix(project):clean compile errors

--- system/ble/controller/ll/module/conn/conn.c
+++ system/ble/controller/ll/module/conn/conn.c
@@ -157,4 +157,9 @@ int ble_ll_enter_connection_state(ble_ll_event_e event)
 	LOG_TRACE(LL_LOG_TRACE,"enter connection state",0,0)
     return 1;
 }
+#else
+int ble_ll_enter_connection_state(ble_ll_event_e event)
+{
+    return 0;
+}
 #endif/*BLE_SUPPORT_CONN*/
