acf308d2de1f631f39b43229ea456ebedf1a07e6
Tianxiang Huang <tianxiang.huang@telink-semi.com>
2026-01-12 20:16:42 +0800

    feature(debug):adv debug arch in ll

---
+++ system/ble/controller/ll/ll_test.h
@@ -0,0 +1,13 @@
+/*
+ * ll_test.h
+ *
+ *  Created on: 2026年1月12日
+ *      Author: Admin
+ */
+
+#ifndef BLE_CONTROLLER_LL_LL_TEST_H_
+#define BLE_CONTROLLER_LL_LL_TEST_H_
+
+
+
+#endif /* BLE_CONTROLLER_LL_LL_TEST_H_ */
---
+++ system/ble/controller/ll/module/conn/test.c
@@ -0,0 +1,8 @@
+/*
+ * test.c
+ *
+ *  Created on: 2026年1月12日
+ *      Author: Admin
+ */
+
+
